289becef9acf244110a42b5b3f91342e7a6c42ea
f6ulld <f.6ulld@gmail.com>
2024-03-22 10:28:59 +0900

    finish project

--- js/custom.js
+++ js/custom.js
@@ -196,20 +196,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
   workAnimation();
 
-  function serviceAnimation() {
+  const pcDataSpeed = ["500", "400", "800", "600"];
+  const mobileDataSpeed = ["150", "180", "250", "180"];
+
+  function serviceAnimation(speed) {
     const arrow_elmts = gsap.utils.toArray(".service-arrow");
 
-    arrow_elmts.forEach((num) => {
+    arrow_elmts.forEach((num, i) => {
+      console.log(speed[i]);
       const data_speed = num.getAttribute("data-speed");
 
       tl.from(num, {
         scrollTrigger: commonScrollTrigger.service,
-        x: -data_speed,
+        x: -speed[i],
       });
     });
   }
 
-  serviceAnimation();
+  serviceAnimation(mobileDataSpeed);
 
   function footerAnimation() {
     const letter_elmts = gsap.utils.toArray(".footer-wrapper span");
@@ -230,8 +234,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
   if (wWidth > 1300) {
     headerAnimation(-70);
+    serviceAnimation(pcDataSpeed);
   } else {
     headerAnimation(0);
+    serviceAnimation(mobileDataSpeed);
   }
 
   // =================================
